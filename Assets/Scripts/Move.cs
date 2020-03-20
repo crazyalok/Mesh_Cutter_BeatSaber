7285730b5f25c660ba94b2a2d9b7d3031bff2fa4
crazyalok <alokmaurya159@gmail.com>
2020-03-21 01:04:58 +0530

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float speed = 3f;
+    public static bool stop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stop == false)
+        {
+            var temp = transform.position.z;
+            temp = temp + Time.deltaTime * speed * 1;
+            transform.position = new Vector3(0, 0, temp);
+            //transform.position = new Vector3(speed*Time.deltaTime*3f, 0, 0);
+        }
+        else
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Saber.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saber : MonoBehaviour {
+
+	public Material capMaterial;
+	public int count = 0;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	void Update(){
+
+		/*if(count < 1){
+			RaycastHit hit;
+
+			if(Physics.Raycast(transform.position, transform.forward, out hit)){
+				count = 1;
+				GameObject victim = hit.collider.gameObject;
+
+				GameObject[] pieces = vrarmrjuet.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+				if (!pieces[1].GetComponent<Rigidbody>())
+				{
+					Move.stop = true;
+					pieces[1].AddComponent<Rigidbody>();
+				}
+				Destroy(pieces[1], 1);
+			}
+
+		}*/
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Entered");
+		GameObject victim = other.gameObject;
+
+		GameObject[] pieces = vrarmrjuet.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+		if (!pieces[1].GetComponent<Rigidbody>())
+		{
+			Move.stop = true;
+			pieces[1].AddComponent<Rigidbody>();
+		}
+		Destroy(pieces[1], 1);
+	}
+
+	void OnDrawGizmosSelected() {
+
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
+		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.5f);
+
+	}
+
+}
